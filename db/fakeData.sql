ae3999ed16318e4f501088b4fa059527c19fc5e3
tpoulsen93 <t-poulsen@hotmail.com>
2023-05-07 21:32:16 -0600

    added fake data sql script

---
+++ db/fakeData.sql
@@ -0,0 +1,11 @@
+-- Fake users
+INSERT INTO users (first_name, last_name, phone, email, password)
+    VALUES ('taylor', 'poulsen', '2081234567', 'tp@gmail.com', 'secret');
+
+INSERT INTO users (first_name, last_name, phone, email, password)
+    VALUES ('trevor', 'cooper', '2082345678', 'tc@gmail.com', 'secret');
+
+
+-- Fake businesses
+INSERT INTO businesses (name, address, phone)
+    VALUES ('Fake Business', '1234 W Fake St.', '2083456789')
